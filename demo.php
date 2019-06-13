38af091d93daf28c110d8fa3b72780e5d8775d78
卢绍明 <lusm@sz-bcs.com.cn>
2019-06-13 12:15:57 +0800

    init

---
+++ demo.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shannon
+ * Date: 2019/6/13
+ * Time: 12:11
+ */
+require_once 'vendor/autoload.php';
+use Shannon\Permission\Tools;
+
+Tools::hello();
---
+++ src/Shannon/Permission/Tools.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shannon
+ * Date: 2019/6/13
+ * Time: 11:50
+ */
+namespace Shannon\Permission;
+
+class Tools
+{
+    public static function hello()
+    {
+        echo "hello world";
+    }
+}
